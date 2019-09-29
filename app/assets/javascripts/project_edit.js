60a53e5783ffd10012597e39d096e089cc9e5457
kyo8300 <kametyou777@gmail.com>
2019-09-29 19:49:14 +0800

    fixed user function

--- app/assets/javascripts/project_edit.js
+++ app/assets/javascripts/project_edit.js
@@ -1,60 +1,55 @@
 //プロジェクトタイトルのリアルタイム文字数カウント
-function ShowLength(str){
-    document.getElementById("wordcnt").innerHTML = str.length + '/30';
+function ShowLength(str) {
+  document.getElementById('wordcnt').innerHTML = str.length + '/30';
 }
 
 //PCのときプロジェクトBASICにボーダーを追加する機能
-function BorderDevice(){
-    let pc = document.getElementsByClassName("project-label");
-    if(window.matchMedia( "(min-width: 576px)" ).matches){
-        for(let i = 0; i < pc.length; i++){
-          pc.item(i).classList.add('border-right');
-        }
-    } else{
-        for(let i = 0; i < pc.length; i++){
-          pc.item(i).classList.remove('border-right');
-        }
+function BorderDevice() {
+  let pc = document.getElementsByClassName('project-label');
+  if (window.matchMedia('(min-width: 576px)').matches) {
+    for (let i = 0; i < pc.length; i++) {
+      pc.item(i).classList.add('border-right');
     }
+  } else {
+    for (let i = 0; i < pc.length; i++) {
+      pc.item(i).classList.remove('border-right');
+    }
+  }
 }
 
 //リターンがlimitかどうか
-function LimitClick(){
-    let limit = document.getElementById('togglelimit');
-    if( limit.style.display == 'none' ){
-        limit.style.display = 'inline';
-    } else{
-        limit.style.display = 'none';
-    }
+function LimitClick() {
+  let limit = document.getElementById('togglelimit');
+  if (limit.style.display == 'none') {
+    limit.style.display = 'inline';
+  } else {
+    limit.style.display = 'none';
+  }
 }
 
 //画像のプレビュー
-$(function(){
-    $fileField = $('#file')
+$(document).on('turbolinks:load', function() {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
 
-    // 選択された画像を取得し表示
-    $($fileField).on('change', $fileField, function(e) {
-      file = e.target.files[0]
-      reader = new FileReader(),
-      $preview = $("#img_field");
+      reader.onload = function(e) {
+        $('#avatar_img_prev').attr('src', e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
 
-      reader.onload = (function(file) {
-        return function(e) {
-          $preview.empty();
-          $preview.append($('<img>').attr({
-            src: e.target.result,
-            width: "100%",
-            class: "preview",
-            title: file.name
-          }));
-        };
-      })(file);
-      reader.readAsDataURL(file);
-    });
+  $('#post_img').change(function() {
+    $('#avatar_img_prev').removeClass('d-none');
+    $('.avatar_present_img').remove();
+    readURL(this);
   });
+});
 
 //Reward/Edit時に商品がLimitからUnlimitedに変わったことを反映させる機能
-function CheckLimit(){
-    if(!document.getElementById('customCheck1').checked){
-        document.getElementById('togglelimit').value = 0;
-    }
+function CheckLimit() {
+  if (!document.getElementById('customCheck1').checked) {
+    document.getElementById('togglelimit').value = 0;
+  }
 }
